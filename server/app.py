05108fd7e9fc347521fa1e3f679c7bf3ca9f2fd1
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-02-14 08:57:59 +0300

    server initial commit

---
+++ server/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from flask import Flask, make_response, request, jsonify
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from werkzeug.exceptions import NotFound
+
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///message.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+api= Api(app)

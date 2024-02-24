2bb7f8e631ca0b458d95602eb3f1d9983df1a660
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-02-24 23:12:13 +0300

    added tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1 className="text-3xl font-bold underline text-cyan-600">Welcome to our website!</h1>
     </div>
   );
 }
